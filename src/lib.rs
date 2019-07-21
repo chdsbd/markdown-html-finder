2c27858c951b164428e17bcfba24f7d33d90bf7a
Christopher Dignam <chris@dignam.xyz>
2019-07-20 20:13:37 -0400

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,12 @@ use pulldown_cmark::{Event, Options, Parser};
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-
 #[pyfunction]
 /// Find positions of html nodes in a markdown file
-/// 
+///
 /// Returns a list of tuples of start and end positions
-fn find_html_positions_py(markdown: &str) -> PyResult<Vec<(usize,usize)>> {
-     Ok(find_html_positions(markdown))
+fn find_html_positions_py(markdown: &str) -> PyResult<Vec<(usize, usize)>> {
+    Ok(find_html_positions(markdown))
 }
 
 /// Module for finding HTML from markdown
@@ -20,17 +19,15 @@ fn markdown_html_finder(_py: Python, m: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
-
-
-fn find_html_positions(markdown: &str) -> Vec<(usize,usize)> {
-    Parser::new_ext(markdown, Options::empty()).into_offset_iter()
+fn find_html_positions(markdown: &str) -> Vec<(usize, usize)> {
+    Parser::new_ext(markdown, Options::empty())
+        .into_offset_iter()
         .filter(|(event, _range)| match event {
             Event::Html(..) | Event::InlineHtml(..) => true,
             _ => false,
         })
-        .map(|(_event, range)|  {
-            (range.start,range.end)
-        }).collect()
+        .map(|(_event, range)| (range.start, range.end))
+        .collect()
 }
 
 #[cfg(test)]
@@ -39,10 +36,7 @@ mod tests {
     #[test]
     fn find_html_positions_simple() {
         let source = "1234567<!-- comment text --> 123";
-        let expected = vec![(
-            7,
-            28
-        )];
+        let expected = vec![(7, 28)];
         assert_eq!(find_html_positions(source), expected);
     }
     #[test]
@@ -50,10 +44,7 @@ mod tests {
         let source = r##"testing
 <p align=center><img src="https://github.com/chdsbd/kodiak/raw/master/assets/logo.png" alt="" width="200" height="200"><!-- comment-a --></p><!-- comment-b -->test
 hello"##;
-        let expected = vec![(8, 172),(
-            172,
-            177
-        )];
+        let expected = vec![(8, 172), (172, 177)];
         assert_eq!(find_html_positions(source), expected);
     }
     #[test]
@@ -62,7 +53,7 @@ hello"##;
 ```
 <-- foo -->
 ```
-"##;    
+"##;
         let expected: Vec<(usize, usize)> = vec![];
         assert_eq!(find_html_positions(source), expected);
     }
